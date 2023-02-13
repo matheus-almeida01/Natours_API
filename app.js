9fb056e9703041c82d05a1e04f4371bf1aabec2a
matheusalmeida01 <matheus.almeida@think.br.com>
2023-02-13 19:29:37 -0300

    middlewares

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const fs = require('fs')
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+// 1) MIDDLEWARES
+app.use(morgan('dev'))
+
 app.use(express.json())
 
+app.use((req, res, next) => {
+    console.log('Hello from the middleware')
+    next()
+})
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+    next()
+})
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tour-simple.json`))
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
     res.status(200).json({ 
@@ -87,6 +103,8 @@ const deleteTour = (req, res) => {
     })
 }
 
+// 3) ROUTES
+
 app
     .route('/api/v1/tours')
     .get(getAllTours)
@@ -96,6 +114,8 @@ app
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour)
+
+// 4) START SERVER
 
 const port = 3000
 app.listen(port, () => {
